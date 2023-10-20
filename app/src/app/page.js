db34ef6ceaefe3424de107ac0bd3d8dcd6707e44
Vanessa Dickey <vcdickey@gmail.com>
2023-10-20 06:43:58 -0400

    added second pic

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -157,24 +157,15 @@ function BlogPage(){
                 />
            
               <div class="card-body">
-                <h4><a href="https://vanessadickey.github.io/bootcamp-blog/week-1.html">Week 1</a></h4>
+                <h4><a href="https://vanessadickey.github.io/silly-story-generator/">Silly Story Generator</a></h4>
                 <p class="card-text text-truncate-11">
                  
-Week 1
-
-- My first week was about three times more difficult than I expected.  I'd consider the stresses of getting the workplaces setup more difficult than our final project from intro to web development.  Debugging is something I've had experience with and had success with but git is something that has confused me for years and I was never able to surmount that mountain to understand how to use it properly.  
-- I am excited to learn backend development since that will be brand new to me and I think it's a career path I would like to pursue.  The visual creativity of frontend development is something I find especially exhausting.  I want to see what the backend entails.
-- Hm....it's hard to think of something to share about myself that can only be learned by reading this blog since a lot of my life is shared online.  I just snapped my fingers 15 times on this date 8/25/23.  No one knows that except for me and you now, dear reader! :laughing:
-
-    Here is an actual variable I used in my Intro to Web Dev class this summer:
-```
-const maskingString = "I believe in the mind reader!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-```
+                A fun app that generates random silly stories.
 
                 </p>
               </div>
               <div class="card-footer">
-                <a href="https://vanessadickey.github.io/bootcamp-blog/week-1.html">more...</a>
+                <a href="https://github.com/vanessadickey/silly-story-generator">Github</a>
               </div>
             </div>
               </div>
@@ -184,7 +175,7 @@ const maskingString = "I believe in the mind reader!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
           <div class="card h-100">
             
               <img class="card-header"
-                src="img\dunetocat.png"
+                src="https://images.pexels.com/photos/1037993/pexels-photo-1037993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               />
             
             <div class="card-body">
