3c23ffa6782a009558fb03ed78b3f1cf55d1340e
Vanessa Dickey <vcdickey@gmail.com>
2023-10-21 20:44:40 -0400

    made firebase.js

---
+++ src/app/firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'your-api-key',
+  authDomain: 'your-auth-domain',
+  projectId: 'your-project-id',
+  storageBucket: 'your-storage-bucket',
+  messagingSenderId: 'your-messaging-sender-id',
+  appId: 'your-app-id',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
